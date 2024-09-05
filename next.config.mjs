f41cc959103cd0b3335211079a23c1c81005555f
doctorbond2 <michael.rostlund@hotmail.com>
2024-09-05 19:43:33 +0200

    asd

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["a.storyblok.com"],
-        formats: ["image/avif", "image/webp"],
+  images: {
+    domains: ['a.storyblok.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
+
+  // Add the redirects function here
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
       },
+    ];
+  },
 };
 
 export default nextConfig;
